b21604e696f2c24804f7d92f7f117bf738251ad6
Vahid Taghizadeh <vahid@brenger.nl>
2020-03-31 22:50:19 +0200

    Updates

---
+++ precache-manifest.31b0b098634fac1c100b2dea358f7fea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f31ed03b5d295c1168009c90ca71d2",
+    "url": "/dayere/index.html"
+  },
+  {
+    "revision": "ecdccd2f7787786ff8c2",
+    "url": "/dayere/static/css/main.833b84b3.chunk.css"
+  },
+  {
+    "revision": "47e7a6f19270b4c64226",
+    "url": "/dayere/static/js/2.713ead58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dayere/static/js/2.713ead58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdccd2f7787786ff8c2",
+    "url": "/dayere/static/js/main.366dfa5e.chunk.js"
+  },
+  {
+    "revision": "eb673d088cafcbc30d9b",
+    "url": "/dayere/static/js/runtime-main.99b434ca.js"
+  },
+  {
+    "revision": "ad4f4a4adbfe3a28b2a9042de6a4c824",
+    "url": "/dayere/static/media/cover2.ad4f4a4a.jpg"
+  }
+]);
